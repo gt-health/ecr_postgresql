b250fbbb9072b5f11cff0fc6db979a77a08eb660
Myung Choi <myungchoi@gmail.com>
2019-02-19 03:26:09 -0500

    just create database and schema

--- loadDb.sh
+++ loadDb.sh
@@ -3,5 +3,6 @@
 mkdir -p /opt/data
 psql -U postgres -d postgres -a -f /opt/data/create_user.sql
 psql -U postgres -d postgres -a -f /opt/data/create_database.sql
+psql -U ecr_user -d ecr < /opt/data/create_schema.sql
 #psql -U ecr_user -d ecr < /opt/data/ecrdb.bk
 rm -rf /opt/data/*
