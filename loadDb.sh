54350018d1415121258b2fc2a16c3a2c6a6c46e4
Myung Choi <myungchoi@gmail.com>
2020-04-21 17:11:22 -0400

    added to use environment variable for password set

--- loadDb.sh
+++ loadDb.sh
@@ -4,5 +4,8 @@ mkdir -p /opt/data
 psql -U postgres -d postgres -a -f /opt/data/create_user.sql
 psql -U postgres -d postgres -a -f /opt/data/create_database.sql
 psql -U ecr_user -d ecr < /opt/data/create_schema.sql
+if [[ $ECR_USER_PASSWORD ]]; then
+  psql -c "ALTER USER ecr_user PASSWORD '$ECR_USER_PASSWORD';"
+fi
 #psql -U ecr_user -d ecr < /opt/data/ecrdb.bk
 rm -rf /opt/data/*
